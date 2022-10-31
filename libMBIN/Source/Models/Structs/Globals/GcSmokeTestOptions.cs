44351ec2bd46a04be97dee1d59d86d92f0561a4e
EthanRDoesMC <ethanrdoesmc@gmail.com>
2022-10-31 01:17:22 -0500

    four matching smoke test values

--- libMBIN/Source/Models/Structs/Globals/GcSmokeTestOptions.cs
+++ libMBIN/Source/Models/Structs/Globals/GcSmokeTestOptions.cs
@@ -2,17 +2,18 @@
 {
     public class GcSmokeTestOptions : NMSTemplate
     {
-        public float Unknown0; // 20.0
-        public float Unknown4; // 0.1
-        public float Unknown8; // 4.5
-        public float UnknownC; // 5
+        /* 0x00 */ public float InitialPause; // 20.0
+        /* 0x04 */ public float SmokeTestFlashTimeDuration; // 0.1
+        /* 0x08 */ public float CameraHeight; // 4.5
+        /* 0x0C */ public float CameraMoveSpeed; // 5
+        
+        // have these changed? are smoke tests in use? 
+        /* 0x10 */ public int Unknown10; // 0 (unused?)
+        /* 0x14 */ public int Unknown14; // 0
+        /* 0x18 */ public int Unknown18; // 0 (unused?)
+        /* 0x1C */ public int Unknown1C; // 25 (unused?)
 
-        public int Unknown10; // 0 (unused?)
-        public int Unknown14; // 0
-        public int Unknown18; // 0 (unused?)
-        public int Unknown1C; // 25 (unused?)
-
-        public float Unknown20; // 0.3
-        public float Unknown24; // 60.0
+        /* 0x20 */ public float Unknown20; // 0.3
+        /* 0x24 */ public float Unknown24; // 60.0
     }
 }
